e802a69f26a767bfe9ec1bcc9604bb8c2912774c
Saba Ansari <saba.ansari4647@gmail.com>
2024-05-14 10:34:52 -0700

    second commit

--- app/src/main/java/ir/shariaty/x_o/MainActivity.kt
+++ app/src/main/java/ir/shariaty/x_o/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ir.shariaty.x_o.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -16,11 +17,33 @@ class MainActivity : AppCompatActivity() {
         binding.playOfflineBtn.setOnClickListener {
             createOfflineGame();
         }
+        binding.playOnlineBtn.setOnClickListener {
+            createOnlineGame();
+        }
+        binding.joinOnlineBtn.setOnClickListener {
+            joinOnlineGame();
+        }
     }
+
+    private fun joinOnlineGame() {
+        TODO("Not yet implemented")
+    }
+
     fun createOfflineGame() {
         GameData.saveGameModel(
             GameModel(
                 gameStatus = GameStatus.JOINED
+            )
+        )
+        startGame()
+    }
+
+    fun createOnlineGame(){
+        GameData.myID = "X"
+        GameData.saveGameModel(
+            GameModel(
+                gameStatus = GameStatus.CREATED,
+                gameId = Random.nextInt(1000,9999).toString()
             )
         )
         startGame()
